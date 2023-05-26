827235a9d95214fa6a258f5a501e5bfa411aca48
j1c <jaewonc78@gmail.com>
2023-05-26 15:51:26 -0400

    update

--- docs/2023-05-06-multiway-dcorr.py
+++ docs/2023-05-06-multiway-dcorr.py
@@ -40,8 +40,14 @@ lookup = {key: val for key, val in zip(enc.get_feature_names_out(), range(12))}
 # %%
 groups = [
     ["Sex_Female", "Sex_Male", "Diet_Control", "Allele_HN", "Allele_Non-HN"],
+    ["Sex_Female", "Sex_Male", "Diet_Control", "Allele_HN"],
+    ["Sex_Female", "Sex_Male", "Diet_Control", "Allele_Non-HN"],
     ["Sex_Female", "Sex_Male", "Diet_Control"],
-    ["Diet_Control", "Allele_HN", "Allele_Non-HN"],
+    ["Sex_Female", "Sex_Male", "Diet_Control"],
+    ["Sex_Female", "Diet_Control", "Allele_HN"],
+    ["Sex_Male", "Diet_Control", "Allele_HN"],
+    ["Sex_Female", "Diet_Control", "Allele_Non-HN"],
+    ["Sex_Male", "Diet_Control", "Allele_Non-HN"],
     ["Diet_Control"],
 ]
 
@@ -53,87 +59,6 @@ for i in [3, 2]:
 genotype_groups = [list(i) for i in genotype_groups]
 
 TEST_LIST = [i + j for i in genotype_groups for j in groups]
-
-# %%
-
-res = []
-
-for test in TEST_LIST:
-    lookup_codes = [lookup[i] for i in test]
-    test_multiway_table = multiway_table[:, lookup_codes]
-    maximum = np.max(test_multiway_table.sum(axis=1))
-
-    idx = test_multiway_table.sum(axis=1) == maximum
-
-    x = vols[idx]
-    y = test_multiway_table[idx]
-
-    stat, pval = Dcorr().test(x, y)
-
-    apoe2 = True if "Genotype_APOE22" in test else False
-    apoe3 = True if "Genotype_APOE33" in test else False
-    apoe4 = True if "Genotype_APOE44" in test else False
-    sex = True if "Sex_Female" in test else False
-    allele = True if "Allele_HN" in test else False
-    multiway = True if sex or allele else False
-
-    res.append([multiway, apoe2, apoe3, apoe4, sex, allele, pval])
-
-
-# %%
-columns = ["Multiway", "APOE2", "APOE3", "APOE4", "Sex", "Allele", "pval"]
-df = pd.DataFrame(res, columns=columns)
-
-df["K"] = (
-    (df[["APOE2", "APOE3", "APOE4"]].sum(axis=1)) * (df["Sex"] + 1) * (df["Allele"] + 1)
-)
-
-df.sort_values(by="K", inplace=True, ascending=False)
-
-i_new = []
-for idx, row in df.iterrows():
-    index = ""
-    for col in ["K", "Multiway", "APOE2", "APOE3", "APOE4", "Sex", "Allele"]:
-        if col == "K":
-            index += str(row[col]) + " | "
-        else:
-            if row[col]:
-                index += "X | "
-            else:
-                index += "  | "
-    i_new.append(index)
-
-df.index = i_new
-
-_, corrected_pvals, _, alpha = multipletests(
-    df[["pval"]].values.ravel(), method="bonferroni"
-)
-df["corrected_pval"] = corrected_pvals
-
-pvalues = df.copy()[["corrected_pval"]]
-mask = np.select([pvalues < alpha, pvalues >= alpha], ["X", ""], default=pvalues)
-
-df.to_csv("../outs/2021-05-06-multiway-dcorr-whole-vols.csv")
-
-# %%
-fig, ax = plt.subplots(figsize=(2, 4), dpi=300)
-
-sns.heatmap(
-    pvalues.transform("log10"),
-    ax=ax,
-    annot=mask,
-    fmt="",
-    square=False,
-    linewidths=0.5,
-    center=alpha,
-    cbar_kws={"ticks": np.log10([0.01, 0.05, 0.1, 1])},
-    cmap="RdBu",
-)
-
-ax.collections[0].colorbar.set_label("pvalue (log scale, bonferroni-adjusted)")
-ax.collections[0].colorbar.set_ticklabels([0.01, 0.05, 0.1, 1])
-
-ax.set_yticklabels(ax.get_yticklabels(), ha="right", fontdict={"family": "monospace"})
 
 # %%
 # Run node wise
